5a1934ec39d9cd2f8e67c27eac2f719eabac14f0
Yshika Agarwal <31753378+Yshika@users.noreply.github.com>
2019-11-25 00:00:12 +0530

    This is the machine learning file

---
+++ ml.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+df=pd.read_csv("Diabetes_Dataset.csv")
+X=df.drop("Outcome",axis=1)
+y=df['Outcome']
+from sklearn.linear_model import LogisticRegression
+model=LogisticRegression()
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.30,random_state=42)
+model.fit(X_train,y_train)
+model.score(X_test,y_test)
+
+
+def pr(preg,glu,blood,skin,ins,bmi,pdf,age):
+    a=0
+    l=pd.DataFrame((np.array([preg,glu,blood,skin,ins,bmi,pdf,age])).reshape(1,-1))
+    a=model.predict(l)
+    a=int(a)
+    if a==0:
+        return "No"
+    else:
+        return "Yes"
